945c1d1bfb7cabdea2a8693f80c3e2a9d6701959
RenckMat <110570477+RenckMat@users.noreply.github.com>
2024-12-09 09:42:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btnEntrar").addEventListener("click", function() {
+    let username = document.getElementById("usr").value
+    let password = document.getElementById("passwd").value
+    alert(username, password)
+})
