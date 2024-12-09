fe0abacbdb02c64b9d072c2f391b18964109a1c1
RenckMat <110570477+RenckMat@users.noreply.github.com>
2024-12-09 10:05:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-alert("teste")
-console.log("testando pela segunda vez")
-prompt("qual seu nome?")
+document.getElementById("btnEntrar").addEventListener("click", function() {
+    let username = document.getElementById("usr").value
+    let password = document.getElementById("passwd").value
+    alert(username, password)
+})
