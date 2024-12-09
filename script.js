e11efb61cf091508b9fa91ae0d448b56c2603540
RenckMat <110570477+RenckMat@users.noreply.github.com>
2024-12-09 09:57:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.getElementById("btnEntrar").addEventListener("click", function() {
-    let username = document.getElementById("usr").value
-    let password = document.getElementById("passwd").value
-    alert(username, password)
+    alert("esta funcionando o click");
 })
