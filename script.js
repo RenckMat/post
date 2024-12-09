4304e4039bd0d5d590cfd799bcaa1f4ed734a894
RenckMat <110570477+RenckMat@users.noreply.github.com>
2024-12-09 10:35:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("btnEntrar").addEventListener("click", function() {
-    let username = document.querySelectorAll(".campoTexto")[1].value
-    let password = document.querySelectorAll(".campoTexto")[2].value
-    alert(`Username: ${username} - Password: ${password}`)
+    let username = btoa(document.querySelectorAll(".campoTexto")[1].value)
+    let password = btoa(document.querySelectorAll(".campoTexto")[2].value)
+    let http = encode(`localhost:8080?u=${username}&p=${password}`)
+    alert(http)
 })
